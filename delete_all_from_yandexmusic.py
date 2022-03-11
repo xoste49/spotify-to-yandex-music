87a870157787a2661f2785f3c0b396e63bcd920f
xoste49 <xoste49@gmail.com>
2022-03-11 15:51:00 +0300

    Add progress bar

--- delete_all_from_yandexmusic.py
+++ delete_all_from_yandexmusic.py
@@ -3,19 +3,20 @@
 """
 
 import os
-from pprint import pprint
 
-from yandex_music import Client
 from dotenv import load_dotenv
+from progress.bar import Bar
+from yandex_music import Client
 
 load_dotenv()
 
 client = Client(os.environ['YANDEX_MUSIC_TOKEN']).init()
 
 print('Всего треков на удаление:', len(client.users_likes_tracks().tracks))
-for track in client.users_likes_tracks().tracks:
-    success = client.users_likes_tracks_remove(track.id)
-    answer = f'{track.id} Удален' if success else f'{track.id} Произошла ошибка'
-    print(answer)
+with Bar('Удаление', max=len(client.users_likes_tracks().tracks)) as bar:
+    for track in client.users_likes_tracks().tracks:
+        success = client.users_likes_tracks_remove(track.id)
+        #answer = f'{track.id} Удален' if success else f'{track.id} Произошла ошибка'
+        bar.next()
 
 print('Осталось треков:', len(client.users_likes_tracks().tracks))
